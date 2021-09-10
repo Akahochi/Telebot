ad0eb9fa016a6a3790f5788c87c962947f457238
Akahochi <88187231+Akahochi@users.noreply.github.com>
2021-09-10 14:37:27 +0000

    Add files via upload

--- bl/registration.py
+++ bl/registration.py
@@ -7,22 +7,53 @@ from telebot import types
 from bl.constants import users, users_todo
 from bot import bot
 
+from sqlalchemy import create_engine, Column, Integer, String, and_
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# """
+# SQLite table for registration data
+# """
+# conn = sqlite3.connect('registration.db', check_same_thread=False)
+# cur = conn.cursor()
+# cur.execute(
+#     """
+#     CREATE TABLE IF NOT EXISTS registration(
+#     user_id INTEGER PRIMATY KEY,
+#     user_id INT NOT NULL,
+#     name TEXT,
+#     surname TEXT,
+#     age INT);
+#     """
+# )
+# conn.commit()
+
 """
-SQLite table for registration data
+SQLAlchemy table for registration data
 """
-conn = sqlite3.connect('registration.db', check_same_thread=False)
-cur = conn.cursor()
-cur.execute(
-    """ 
-    CREATE TABLE IF NOT EXISTS registration(
-    user_id INTEGER PRIMATY KEY,
-    user_id INT NOT NULL, 
-    name TEXT, 
-    surname TEXT, 
-    age INT);
-    """
-)
-conn.commit()
+engine = create_engine("sqlite+pysqlite:///sqlalchemy_registration.db", echo=True, future=True)
+Session = sessionmaker(engine)
+Base = declarative_base()
+
+
+class Registration(Base):
+    __tablename__ = "registration"
+
+    id_db = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    name = Column(String(64), nullable=False)
+    surname = Column(String(64), nullable=False)
+    age = Column(Integer, nullable=False)
+
+    def __str__(self):
+        return f"user_id:{self.user_id}, " \
+               f"name:{self.name}, " \
+               f"surname:{self.surname}," \
+               f"age:{self.age},"
+
+
+Base.metadata.create_all(engine)
+
 
 
 def is_valid_name_surname(name_surname):
@@ -59,10 +90,36 @@ def get_surname(message):
         bot.register_next_step_handler(message, get_surname)
 
 
-"""
-Function with writing data to SQLite database
-"""
+# """
+# Function with writing data to SQLite database
+# """
+#
+#
+# @bot.callback_query_handler(func=lambda call: call.data.startswith("reg_"))
+# def callback_worker(call):
+#     user_id = call.from_user.id
+#     user_d = {}
+#     user_d["user_id"] = user_id
+#     user_d["name"] = users[user_id]["name"]
+#     user_d["surname"] = users[user_id]["surname"]
+#     user_d["age"] = users[user_id]["age"]
+#
+#     if call.data == "reg_yes":
+#         bot.send_message(user_id, "Thank you, I will remember!")
+#         # pretend that we save in database
+#         cur.execute("""
+#         INSERT INTO registration (user_id, name, surname, age)
+#         VALUES (:user_id,:name,:surname,:age)""", user_d)
+#         conn.commit()
+#     elif call.data == "reg_no":
+#         # remove user
+#         users_todo.pop(user_id, None)
+#         render_initial_keyboard(user_id)
 
+
+"""
+Function with writing data to SQLAlchemy database
+"""
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("reg_"))
 def callback_worker(call):
@@ -72,19 +129,19 @@ def callback_worker(call):
     user_d["name"] = users[user_id]["name"]
     user_d["surname"] = users[user_id]["surname"]
     user_d["age"] = users[user_id]["age"]
-
     if call.data == "reg_yes":
         bot.send_message(user_id, "Thank you, I will remember!")
         # pretend that we save in database
-        cur.execute("""
-        INSERT INTO registration (user_id, name, surname, age)
-        VALUES (:user_id,:name,:surname,:age)""", user_d)
-        conn.commit()
+        session = Session()
+        new_user = Registration(user_id=user_d["user_id"], name=user_d["name"], surname=user_d["surname"], age=user_d["age"])
+        session.add(new_user)
+        session.flush()
+        session.commit()
+        session.close()
     elif call.data == "reg_no":
         # remove user
         users_todo.pop(user_id, None)
         render_initial_keyboard(user_id)
-
 
 # """
 # Function with writing data to csv
